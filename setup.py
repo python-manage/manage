6c39e0ddb50a0696f812dc0833ffc43b00e85231
Bruno Rocha <rochacbruno@gmail.com>
2016-06-26 22:37:41 -0300

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='manage',
-    version='0.1.6',
+    version='0.1.7',
     description=(
         "Command Line Manager + Interactive Shell for Python Projects"
     ),
