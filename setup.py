26a5b247c5d878a00db08aa4094218d8cc458e9e
nixocio <nixocio@gmail.com>
2019-10-13 11:30:04 -0400

    Update min required Click version
    Update min required Click version. `Click=>7.0`.
    
    Fix unittests due to changes in click version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'Click>=6.0',
+    'Click>=7.0',
     'PyYAML>=3.11',
     'six',
     'import_string'
